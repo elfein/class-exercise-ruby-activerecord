217c34e563b105438860b4479b5cb8cb33984010
elfein <elfein99@gmail.com>
2018-10-24 19:44:58 -0400

    Add full CRUD on movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,40 @@
 class MoviesController < ApplicationController
   def index
+    @movies = Movie.all
   end
 
   def show
+    @movie = Movie.find(params[:id])
   end
 
   def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.create(movie_params)
+    redirect_to movies_url
   end
 
   def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to movie_url(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_url
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :genre, :year, :synopsis, :picture)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.destroy_all
+
+school = Movie.create(title: 'School of Rock', genre: 'Comedy', year: 2008, synopsis: 'A joy to behold', picture: 'http://www.gstatic.com/tv/thumb/v22vodart/33094/p33094_v_v8_ab.jpg')
+shrek2 = Movie.create(title: 'Shrek 2', genre: 'Comedy', year: 3000, synopsis: 'Cinematic masterpiece', picture: 'https://upload.wikimedia.org/wikipedia/el/thumb/f/ff/Shrek_2.jpg/250px-Shrek_2.jpg')
+killbill = Movie.create(title: 'Kill Bill', genre: 'Action', year: 1800, synopsis: 'Yes', picture: 'https://upload.wikimedia.org/wikipedia/en/thumb/2/2c/Kill_Bill_Volume_1.png/220px-Kill_Bill_Volume_1.png')
